0b3fbafb2cab9054ddd3e9a04cd3802800f034f7
exim13 <exim13@users.noreply.github.com>
2020-01-22 22:33:55 -0800

    Modified to release all pokemon in a box

--- instructions.h
+++ instructions.h
@@ -34,126 +34,43 @@ typedef struct {
     uint16_t duration;
 } command_t;
 
-static const command_t wake_up_hang[] = {
-    { hang,  150 }
-};
 
-static const command_t fly_to_breading_steps[] = {
-    { hang, 50 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_x, 7},
+static const command_t release_poke[] = {
+    { hang, 100 },
+	{ move_forward, 5 },
+    { hang, 30 },
+	{ move_backward, 5 }, 
+    { hang, 30 },
+	{ press_a, 10 },
     { hang, 70 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_a, 5 },
-    { hang, 50 },
-    { press_a, 5 }
-};
-
-static const command_t get_egg_steps[] = {
-    { move_backward, 70 }, // need to tune
-    {reset_view_angle, 5 },
-    { move_right, 10 },
-    { press_a, 5 },
-    { hang, 20 },
-    { press_a, 5 },
-    { hang, 20 },
-    { move_backward, 5 },
-    { hang, 20 },
-    { move_backward, 5 },
-    { hang, 20 },
-    { press_a, 5 },
-    { hang, 70 },
-    { press_a, 5 },
-    { hang, 5 },
-    { move_right, 25 },
-    { hang, 60 },
-    { press_a, 5 },
-    { hang, 70 },
-    { press_a, 5 },
-    { hang, 70 },
-    { press_a, 5 },
-    { hang, 70 },
-};
-
-static command_t swap_slot_2[] = {
-    { move_backward, 5 }, 
-    { hang, 30 }
-};
-
-static command_t swap_slot_3[] = {
-    { move_backward, 5 }, 
-    { hang, 30 }, 
-    { move_backward, 5 },
-    { hang, 30 }
-};
-
-static command_t swap_slot_4[] = {
-    { move_backward, 5 }, 
-    { hang, 30 }, 
-    { move_backward, 5 }, 
-    { hang, 30 }, 
-    { move_backward, 5 }, 
-    { hang, 30 }
-};
-
-static command_t swap_slot_5[] = {
-    { move_forward, 5 }, 
-    { hang, 30 }, 
-    { move_forward, 5 }, 
-    { hang, 30 }
-};
-
-static command_t swap_slot_6[] = {
     { move_forward, 5 },
-    { hang, 30 }
-};
-
-static const command_t* swap_slot[] = {
-    swap_slot_2,
-    swap_slot_3,
-    swap_slot_4,
-    swap_slot_5,
-    swap_slot_6
-};
-
-static const uint8_t swap_slot_size[] = {
-    2, 4, 6, 4, 2
-};
-
-static const command_t swap_pokemon_steps[] = {
-    { press_a, 10 },
-    { hang, 100 },
-    { press_b, 10 },
-    { hang, 100 },
-    { press_b, 10 },
-    { hang, 100 },
-    { press_b, 10 },
-    { hang, 70 },
-    { press_b, 10 },
-    { hang, 70 }
-};
-
-static const command_t breeding_prep_steps[] = {
-    { move_backward, 30 },
-    { hang, 10 },
-    { move_left, 80 },
-    { hang, 5 },
-    { press_plus, 5 },
-    { hang, 5 },
-};
-
-static const command_t next_round_steps[] = {
     { hang, 20 },
-    { press_plus, 5 },
-    { hang, 20 }
+    { move_forward, 5 },
+    { hang, 20 },
+    { press_a, 10 },
+	{ hang, 100 },
+	{ move_forward, 5 },
+    { hang, 20 },
+    { press_a, 10 },
+	{ hang, 100 },
+	{ press_a, 10 },
+	{ hang, 70 },	// one done
+};
+
+static const command_t move_to_next_poke[] = {
+	{ hang, 100 },
+    { move_right, 5 },
+    { hang, 30 },
+};
+
+static const command_t move_to_next_row[] = {
+	{ hang, 100 },
+    { move_right, 5 },
+	{ hang, 30 },
+	{ move_right, 5 },
+	{ hang, 30 },
+	{ move_backward, 5 }, 
+    { hang, 30 },	// next row ready
 };
 
 static const uint16_t breeding_duration = 3000;
